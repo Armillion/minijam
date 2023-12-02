25fb706529766092214e30781a22f3ec8b54d5c7
Armillion <96535145+Armillion@users.noreply.github.com>
2023-12-02 11:16:20 +0100

    Enemy changes

--- Assets/Scripts/AttackChecker.cs
+++ Assets/Scripts/AttackChecker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AttackChecker : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         Entity en = other.gameObject.GetComponent<Entity>();
         if (en != null)
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -33,6 +33,10 @@ public class PlayerMovement : MonoBehaviour
 	public BoolEvent OnCrouchEvent;
 	private bool m_wasCrouching = false;
 
+	[SerializeField] private float knockbackTime = 3f;
+    [SerializeField] private float knockbackTimer = 0f;
+	[SerializeField] private bool isKnockedback = false;
+
 	private void Start()
     {
 		nrJumps = maxNrJumps;
@@ -70,6 +74,20 @@ public class PlayerMovement : MonoBehaviour
         if(m_Grounded && nrJumps != maxNrJumps)
         {
             nrJumps = maxNrJumps;
+        }
+
+		if(isKnockedback)
+		{
+			knockbackTimer += Time.deltaTime;
+			m_AirControl = false;
+
+			if (knockbackTimer > 0.5f
+				&& (m_Grounded || knockbackTimer > knockbackTime))
+            {
+                isKnockedback = false;
+                m_AirControl = true;
+                knockbackTimer = 0f;
+            }
         }
 	}
 
@@ -122,7 +140,9 @@ public class PlayerMovement : MonoBehaviour
 			// Move the character by finding the target velocity
 			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
 			// And then smoothing it out and applying it to the character
-			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+			if(!isKnockedback)
+				m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
 			// If the input is moving the player right and the player is facing left...
 			if (move > 0 && !m_FacingRight)
@@ -158,7 +178,10 @@ public class PlayerMovement : MonoBehaviour
 
 	public void knockback(float force)
 	{
-        Vector3 targetVelocity = new Vector2(force * 100f, 600f);
+		isKnockedback = true;
+		m_AirControl = false;
+		m_Grounded = false;
+        Vector3 targetVelocity = new Vector2(force, 10f);
 		m_Rigidbody2D.velocity = targetVelocity;
     }
 
